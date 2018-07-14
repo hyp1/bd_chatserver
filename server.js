3221935b13bd7b3ec3f55d3a4501ab2a252cc866
robert <robert_schweng@hotmail.com>
2018-07-14 19:04:01 +0200

    branch witch private messages

--- server.js
+++ server.js
@@ -10,17 +10,59 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-io.on('connection', function(socket){
-    console.log('client connected:'+socket.handshake.headers.host);
-    socket.on('chat message', function(msg){
-        console.log('server chat message:'+msg);
-    io.emit('chat message', msg);
-    });
 
-    socket.on('disconnect', function(){
-        console.log('client disconnected:'+socket.handshake.headers.host);
-    });
-});
+
+var clients = {};
+logs=[];
+
+io.sockets.on('connection', function (socket) {
+    var userName;
+  socket.on('connection name',function(user){
+    console.log('ip:'+socket.handshake.address);
+    userName = user.name;
+    clients[user.name] = socket;
+    console.log('clients['+user.name+']');
+    io.sockets.emit('new user', user.name + " has joined.");
+  });
+
+  socket.on('message', function(msg){
+    io.sockets.emit('message', msg);
+    console.log(clients.length,'clients');
+    addUserlist();
+  });
+
+  socket.on('private message', function(msg){
+    console.log(msg);
+    fromMsg = {from:userName, txt:msg.txt}
+   // no client clients[NAME]);
+    if(clients[msg.to]!=undefined)clients[msg.to].emit('private message', fromMsg);
+    else    console.log("no recipient");
+  });
+
+  socket.on('disconnect', function(){
+    console.log('disconnected '+userName);
+    delete clients[userName];
+  });
+
+ 
+}); //userName !!!
+
+function addUserlist(){
+    for (var k in clients){
+        if (clients.hasOwnProperty(k)) {
+            var u
+            io.sockets.emit('userlist', k + " has joined.");
+             console.log("Key is " + k + ", value is" + clients[k].handshake.address);
+        }
+    }
+     
+}
+
+
+function addLog(cmd,msg){
+
+     
+}
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
@@ -31,4 +73,8 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
  
+app.get('/chat.html', function(req, res){
+    res.sendFile(__dirname + '/chat.html');
+});
+
 module.exports = app ;
--- tests/app_test.js
+++ tests/app_test.js
@@ -1,3 +1,7 @@
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
 var server   = require('../server'),
     chai     = require('chai'),
     chaiHTTP = require('chai-http'),
@@ -19,3 +23,108 @@ describe('Basic routes tests', function() {
     })
 
 })
+
+
+
+
+var io = require('socket.io-client');
+
+var socketURL = 'http://'+ip+':'+port;
+
+var options ={
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+var chatUser1 = {'name':'Tom'};
+var chatUser2 = {'name':'Sally'};
+var chatUser3 = {'name':'Dana'};
+
+
+describe("Chat Server",function(){
+    it('Should be able to broadcast messages', function(done){
+        var client1, client2, client3;
+        var message = 'Hello World';
+        var messages = 0;
+      
+        var checkMessage = function(client){
+          client.on('message', function(msg){
+            message.should.equal(msg);
+            client.disconnect();
+            messages++;
+            if(messages === 3){
+              done();
+            };
+          });
+        };
+      
+        client1 = io.connect(socketURL, options);
+        checkMessage(client1);
+      
+        client1.on('connect', function(data){
+          client2 = io.connect(socketURL, options);
+          checkMessage(client2);
+      
+          client2.on('connect', function(data){
+            client3 = io.connect(socketURL, options);
+            checkMessage(client3);
+      
+            client3.on('connect', function(data){
+              client2.send(message);
+            });
+          });
+        });
+      });
+      
+      
+      it('Should be able to send private messages', function(done){
+        var client1, client2, client3;
+        var message = {to: chatUser1.name, txt:'Private Hello World'};
+        var messages = 0;
+      
+        var completeTest = function(){
+          messages.should.equal(1);
+          client1.disconnect();
+          client2.disconnect();
+          client3.disconnect();
+          done();
+        };
+      
+        var checkPrivateMessage = function(client){
+          client.on('private message', function(msg){
+              console.log(message);
+            message.txt.should.equal(msg.txt);
+            msg.from.should.equal(chatUser3.name);
+            messages++;
+            if(client === client1){
+              /* The first client has received the message
+              we give some time to ensure that the others
+              will not receive the same message. */
+              setTimeout(completeTest, 40);
+            };
+          });
+        };
+      
+        client1 = io.connect(socketURL, options);
+        checkPrivateMessage(client1);
+      
+        client1.on('connect', function(data){
+          client1.emit('connection name', chatUser1);
+          client2 = io.connect(socketURL, options);
+          checkPrivateMessage(client2);
+      
+          client2.on('connect', function(data){
+            client2.emit('connection name', chatUser2);
+            client3 = io.connect(socketURL, options);
+            checkPrivateMessage(client3);
+      
+            client3.on('connect', function(data){
+              client3.emit('connection name', chatUser3);
+              client3.emit('private message', message)
+            });
+          });
+        });
+      });
+
+});
+
